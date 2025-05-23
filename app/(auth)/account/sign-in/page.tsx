3b55ffeae1b1242a6f715d8c5c6833a74c041c1f
miguel angel <miguelangeljimenez1047@gmail.com>
2025-05-23 10:55:37 -0500

    FEAT: setup sign in page ✅

---
+++ app/(auth)/account/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignInView } from '@/app/(auth)/account/sign-in/_components/sign-in-view'
+
+export default function SignInPage() {
+  return (
+    <section className='w-full h-screen p-8'>
+      <SignInView />
+    </section>
+  )
+}
