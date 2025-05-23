d77783bf849a315b6bd9fafdc78e7ad1e3f3a7c8
miguel angel <miguelangeljimenez1047@gmail.com>
2025-05-23 10:44:34 -0500

    FEAT: setup home page ✅

---
+++ app/(home)/_components/home-form/home-form.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { z } from 'zod'
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from '@/components/ui/input'
+import { Loader } from 'lucide-react'
+import { toast } from 'sonner'
+import { authService } from '@/services/auth-service'
+import { signInSchema } from '@/schemas'
+import { useRouter } from 'next/navigation'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
+
+export function HomeForm() {
+  const { push } = useRouter()
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: { email: 'guestuser@email.com', password: 'password123' },
+  })
+
+  const { email, password } = form.watch()
+  const isSubmiting = form.formState.isSubmitting
+  const isValidValues = !!email && !!password
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    const { success, message } = await authService.signIn(data)
+    if (!success) return toast.success(message)
+    toast.success('Log in successful!')
+    push(DEFAULT_LOGIN_REDIRECT)
+  })
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={onSubmit}
+        className='w-full grid gap-4'
+      >
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  disabled={isSubmiting}
+                  placeholder='Email'
+                  className='border-accent'
+                  autoComplete='off'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  disabled={isSubmiting}
+                  placeholder='Password'
+                  type='password'
+                  className='border-accent'
+                  autoComplete='off'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button disabled={isSubmiting || !isValidValues}>
+          {isSubmiting && <Loader className='animate-spin' />}
+          {isSubmiting ? 'Signing in...' : 'Sign in'}
+        </Button>
+      </form>
+    </Form>
+  )
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,56 @@
+import { Asset1, Asset2 } from "@/assets/mocks/_index";
+import { DownloadButtons } from "@/components/download-buttons";
+import ImportantNote from "@/components/important-note/important-note";
+import { SignInWithdiscord } from "@/components/sign-in-with-discord";
+import Iphone15Pro from "@/components/ui/iphone-15-pro";
+import { NAME_APP } from "@/constants/misc";
+import Link from "next/link";
+import { HomeForm } from "./_components/home-form";
+
 export default async function Home() {
-  return <main>Home page</main>
+  return <main className='w-full h-screen grid place-items-center relative'>
+      <ImportantNote />
+
+      <section className='flex justify-center items-center w-[935px]'>
+        <div className='w-[380px]'>
+          <div className='w-full h-[580px] relative'>
+            <Iphone15Pro
+              className='top-0 -left-20 absolute size-full'
+              src={Asset2.src}
+            />
+
+            <Iphone15Pro
+              className='top-4 left-4 absolute size-full'
+              src={Asset1.src}
+            />
+          </div>
+        </div>
+
+        <div className='w-[350px] flex flex-col py-8 gap-2'>
+          <div className='w-full border border-accent p-8 flex flex-col gap-2 items-center space-y-4'>
+            <h2 className='text-4xl font-bold pb-5'>{NAME_APP}</h2>
+            <HomeForm />
+
+            <SignInWithdiscord
+              isSeparator
+              separatorPosition='top'
+            />
+          </div>
+
+          <div className='w-full border border-accent p-8 '>
+            <p className='text-sm text-center'>
+              Don&apos;t have an account?{' '}
+              <Link
+                href='/account/sign-up'
+                className='text-sky-500 dark:text-sky-400 hover:underline'
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+
+          <DownloadButtons />
+        </div>
+      </section>
+    </main>
 }
