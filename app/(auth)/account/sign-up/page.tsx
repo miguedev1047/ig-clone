614cb07a497c737f1aab14dba5be4bb407e93e33
miguel angel <miguelangeljimenez1047@gmail.com>
2025-05-23 10:54:59 -0500

    FEAT: add setup sign up page ✅

---
+++ app/(auth)/account/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpView } from '@/app/(auth)/account/sign-up/_components/sign-up-view'
+
+export default function SignUpPage() {
+  return (
+    <section className='w-full h-screen p-8'>
+      <SignUpView />
+    </section>
+  )
+}
