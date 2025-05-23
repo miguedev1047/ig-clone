27af173dc0ce6391ba327e72fb0c4fe1ce7522a3
miguel angel <miguelangeljimenez1047@gmail.com>
2025-05-23 10:53:27 -0500

    FEAT: add constants files ⚒️

---
+++ constants/general.ts
@@ -0,0 +1,18 @@
+export const GENDERS = [
+  {
+    value: 'MALE',
+    label: 'Male',
+  },
+  {
+    value: 'FEMALE',
+    label: 'Female',
+  },
+  {
+    value: 'OTHER',
+    label: 'Other',
+  },
+  {
+    value: 'NONE',
+    label: 'None',
+  },
+]
---
+++ constants/misc.ts
@@ -0,0 +1,3 @@
+export const MAX_UPLOAD_SIZE = 1024 * 1024 * 5
+export const MIN_COUNT = 0
+export const NAME_APP = 'ℑ𝔫𝔰𝔱𝔞𝔤𝔯𝔞𝔪'
