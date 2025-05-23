e0eb9e57f671e79f88819109ffc37162186408d7
miguel angel <miguelangeljimenez1047@gmail.com>
2025-05-23 10:56:12 -0500

    FEAT: add sign in form component 🎨

---
+++ app/(auth)/account/sign-in/_components/sign-in-form/sign-in-form.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { z } from 'zod'
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { signInSchema } from '@/schemas'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { authService } from '@/services/auth-service'
+import { toast } from 'sonner'
+import { Loader } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
+
+export function SignInForm() {
+  const { push } = useRouter()
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: { email: '', password: '' },
+  })
+
+  const isSubmitting = form.formState.isSubmitting
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    const { success, message } = await authService.signIn(data)
+    if (!success) return toast.error(message)
+    toast.success(message)
+    push(DEFAULT_LOGIN_REDIRECT)
+  })
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={onSubmit}
+        className='w-full grid gap-2'
+      >
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  placeholder='Email'
+                  disabled={isSubmitting}
+                  className='border-accent'
+                  autoComplete='off'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  placeholder='Password'
+                  disabled={isSubmitting}
+                  className='border-accent'
+                  autoComplete='off'
+                  type='password'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button
+          disabled={isSubmitting}
+          type='submit'
+        >
+          {isSubmitting && <Loader className='animate-spin' />}
+          {isSubmitting ? 'Sign in...' : 'Log in'}
+        </Button>
+      </form>
+    </Form>
+  )
+}
