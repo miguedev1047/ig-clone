a47c39be3311a1ea4f5be428c9809b8453143cb1
miguel angel <miguelangeljimenez1047@gmail.com>
2025-05-23 10:47:38 -0500

    FEAT: add important note component 🎨

---
+++ components/important-note/important-note.tsx
@@ -0,0 +1,9 @@
+export default function ImportantNote() {
+  return (
+    <div className='fixed top-0 left-0 right-0 z-50  px-6 py-4 bg-destructive'>
+      <p className='text-black text-center'>
+        This is a clone for educational purposes.
+      </p>
+    </div>
+  )
+}
