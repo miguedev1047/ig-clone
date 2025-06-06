d9c3ce90acb512edb1191d2b8067cdeb771f2e00
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-06 10:28:50 -0500

    FEAT: add post not found component 🎨

---
+++ components/post-not-found/post-not-found.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { AlertCircleIcon } from 'lucide-react'
+
+export function PostNotFound() {
+  return (
+    <div className='w-full h-[calc(100vh-8rem)] grid place-items-center'>
+      <div className='text-center space-y-4'>
+        <div className='size-20 p-7 bg-accent rounded-full mx-auto grid place-items-center'>
+          <AlertCircleIcon className='size-full' />
+        </div>
+
+        <h1 className='text-xl font-bold'>Post isn&apos;t available</h1>
+
+        <p className='text-sm'>
+          The link may be broken, or the post may have been removed.
+        </p>
+
+        <Button asChild>
+          <Link href='/feed'>See more on Instagram</Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
