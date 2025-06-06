4d4d2afb78d88ecce35294a6749dc0bc227ee4c7
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-06 10:20:58 -0500

    FEAT: add constants files ⚒️

--- constants/general.ts
+++ constants/general.ts
@@ -16,3 +16,10 @@ export const GENDERS = [
     label: 'None',
   },
 ]
+
+export const PLAYSTORE_URL =
+  'https://play.google.com/store/apps/details?id=com.instagram.android&pli=1'
+export const MICROSOFT_URL =
+  'https://apps.microsoft.com/detail/9nblggh5l9xt?launch=true&mode=full&hl=es-es&gl=co&ocid=bingwebsearch'
+
+export const SKELETON_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
--- constants/misc.ts
+++ constants/misc.ts
@@ -1,3 +1,4 @@
 export const MAX_UPLOAD_SIZE = 1024 * 1024 * 5
 export const MIN_COUNT = 0
 export const NAME_APP = 'ℑ𝔫𝔰𝔱𝔞𝔤𝔯𝔞𝔪'
+export const DEFAULT_IMAGE = 'https://placehold.co/600?text=IG-CLONE'
