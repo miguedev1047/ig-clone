0062a018845fc91cc1762947d0289c5b9f3a4148
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-06 10:46:04 -0500

    FEAT: add account page ✅

---
+++ app/(protected)/edit/account/page.tsx
@@ -0,0 +1,9 @@
+import { EditorSection } from '@/app/(protected)/edit/_components/editor-section'
+
+export default function EditAccountPage() {
+  return (
+    <section className='w-full max-w-[640px] mx-auto p-8 space-y-12'>
+      <EditorSection />
+    </section>
+  )
+}
