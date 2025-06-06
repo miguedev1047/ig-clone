aafb595a5adf605a3af9a29adbdeab235ea91935
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-06 10:47:01 -0500

    FEAT: add profile/user page ✅

---
+++ app/(protected)/[username]/layout.tsx
@@ -0,0 +1,18 @@
+import { AppSidebar } from '@/components/app-sidebar'
+import { DialogPost } from '@/components/dialog-post'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { PostDialogProvider } from '@/providers/post-dialog-provider'
+
+export default function ProfileLayout(props: { children: React.ReactNode }) {
+  const { children } = props
+
+  return (
+    <PostDialogProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className='w-full'>{children}</main>
+        <DialogPost />
+      </SidebarProvider>
+    </PostDialogProvider>
+  )
+}
---
+++ app/(protected)/[username]/page.tsx
@@ -0,0 +1,9 @@
+import { UserSection } from '@/app/(protected)/[username]/_components/user-section/user-section'
+
+export default function ProfilePage() {
+  return (
+    <section className='w-full max-w-[740px] mx-auto px-8 py-10'>
+      <UserSection />
+    </section>
+  )
+}
