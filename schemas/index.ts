5b1f2c84640e81cb923e333768f355eb7d82e4ab
miguel angel <miguelangeljimenez1047@gmail.com>
2025-06-06 10:18:01 -0500

    CHORE: update zod schemas ⚙️

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,28 +1,66 @@
-import { z } from 'zod'
-
-export const signInSchema = z.object({
-  email: z.string().email({ message: 'Invalid email' }),
-  password: z
-    .string()
-    .min(6, { message: 'Password must be at least 6 characters' }),
-})
+import * as z from 'zod'
 
 export const signUpSchema = z.object({
-  name: z.string().min(3, { message: 'Name must be at least 3 characters' }),
-  email: z.string().email({ message: 'Invalid email' }),
+  name: z.string().min(1, { message: 'Name is required' }),
+  email: z.string().email({ message: 'Invalid email address' }),
   password: z
     .string()
-    .min(6, { message: 'Password must be at least 6 characters' }),
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+})
+
+export const loginSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+})
+
+export const signInSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' }),
 })
 
 export const postSchema = z.object({
-  description: z
-    .string()
-    .min(3, { message: 'Description must be at least 3 characters' }),
-  postId: z
-    .string()
-    .min(3, { message: 'Post ID must be at least 3 characters' }),
+  description: z.string().optional(),
   isPrivate: z.boolean(),
   enabledComments: z.boolean(),
   location: z.string().optional(),
+  postId: z.string().optional(),
+  imageUrl: z.string().optional(),
+})
+
+export const commentSchema = z.object({
+  content: z.string().min(1, { message: 'Comment is required!' }),
+  userId: z.string(),
+  postId: z.string(),
+})
+
+export const followingSchema = z.object({
+  followerId: z.string().optional(),
+  followingId: z.string().optional(),
+})
+
+export const savedPostSchema = z.object({
+  userId: z.string().optional(),
+  postId: z.string().optional(),
+})
+
+export const profileSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required!' }),
+  gender: z.string().min(1, {
+    message: 'Gender is required!',
+  }),
+  userId: z.string().min(1, {
+    message: 'User ID is required!',
+  }),
+  caption: z.string().min(10, {
+    message: 'Caption is required!',
+  }),
+  website: z.string().optional(),
+})
+
+export const searchUserSchema = z.object({
+  username: z.string().min(1, { message: 'Username is required!' }),
 })
